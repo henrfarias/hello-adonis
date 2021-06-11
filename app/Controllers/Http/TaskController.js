2b02ea4291eaf47c486e00c784b0e6bf66a412e5
henrfarias <henrique.biasibetti@gmail.com>
2021-06-11 13:27:37 -0300

    feat:end of class 13.

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -8,16 +8,85 @@
  * Resourceful controller for interacting with tasks
  */
 class TaskController {
-  project() {
-    return this.belongsTo('App/Models/Project');
+  /**
+   * Show a list of all tasks.
+   * GET tasks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
   }
 
-  user() {
-    return this.belongsTo('App/Models/User');
+  /**
+   * Render a form to be used for creating a new task.
+   * GET tasks/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
   }
 
-  file() {
-    return this.belongsTo('App/Models/File');
+  /**
+   * Create/save a new task.
+   * POST tasks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single task.
+   * GET tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing task.
+   * GET tasks/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update task details.
+   * PUT or PATCH tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a task with id.
+   * DELETE tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
   }
 }
 
